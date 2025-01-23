359303fa8d6cb687ff143b427913d44174148b44
Shreeram <deepskinweb@gmail.com>
2025-01-23 11:54:37 +0530

    Date 23 JAN 2025

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Navbar'
-import React from 'react'
 import Footer from './Footer'
 
 
